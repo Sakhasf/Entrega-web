0da967d6f84c6f01754b7d1be6621bcc33bbd6ca
Eric <ericetienneortiz@gmail.com>
2023-10-16 22:57:26 -0300

    app dockerizada

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,12 @@ import { Servidor } from './servidores/entities/servidor.entity';
 @Module({
   imports: [WebModule, ServidoresModule, TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'dpg-cd8rsdun6mpnkgit336g-a',
-//  host: 'localhost',
+//    host: 'dpg-cd8rsdun6mpnkgit336g-a',
+//    host: 'localhost',
+      host: 'postgres',
     port: 5432,
     username: 'prueba',
     password: 'djtvpejR2fTfLzWWKUkHa6TragUzeGMB',
-//  password: 'secure',
     database: 'dataprueba',
     entities: [Web, Servidor],
     synchronize: true,
